1cd7122536b31df6145d3f4034fb89ba97b1f379
bingtengaoyu <992899491@qq.com>
2020-05-19 22:21:14 +0800

    提交源码（持续更新中...）

---
+++ layouts/default.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div class="header">
+      fadf
+    </div>
+    <div class="main">
+      <nuxt />
+      <div class="footer">
+        腾宇官网
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.header {
+  line-height: 60px;
+  background: #35495e;
+  color: #fff;
+}
+.main {
+  height: calc(100vh - 125px);
+  background: #e6e6e6;
+}
+.footer {
+  line-height: 60px;
+  text-align: center;
+  background: #35495e;
+  color: #fff;
+}
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
